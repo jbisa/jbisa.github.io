08d4c05b4013c27645b6d923ed7d9d7f5d4684be
Jay Bisa <jaybisa@Jay-Bisas-MBP.hsd1.ma.comcast.net>
2015-06-07 14:31:17 -0400

    Set up single page web app routing

--- js/controllers/wovfnCtrl.js
+++ js/controllers/wovfnCtrl.js
@@ -1,6 +1,28 @@
-var myApp = angular.module('wovfnApp', ['ngResource'])
+/**
+ * Set up AngularJS app
+ */
+var myApp = angular.module('wovfnApp', ['ngResource', 'ngRoute']);
 
-myApp.controller('WOVFNCtrl', ['$scope', function($scope) {
-  $scope.message = 'Hello!';
+/**
+ * Configure the app routes
+ */
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+      // Portfolio
+      .when("/", {templateUrl: "content/portfolio.html", controller: "WovfnCtrl"})
+      .when("/portfolio", {templateUrl: "content/portfolio.html", controller: "WovfnCtrl"})
+      // About
+      .when("/about", {templateUrl: "content/about.html", controller: "WovfnCtrl"})
+      // Contact
+      .when("/contact", {templateUrl: "content/contact.html", controller: "WovfnCtrl"})
+      .otherwise("/404", {templateUrl: "content/404.html", controller: "WovfnCtrl"})
+}]);
+
+
+/**
+ * AngularJS controller which controls logic of app
+ */
+myApp.controller('WovfnCtrl', ['$scope', function($scope) {
+  console.log("Switching pages...");
 
 }]);
