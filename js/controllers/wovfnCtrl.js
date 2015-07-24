d43fe1c589d910f5c4e5e1b91acd67f082eee75b
Jay Bisa <jaybisa@Jay-Bisas-MBP.home>
2015-07-23 20:36:53 -0400

    Fix active tab when page is reloaded.

--- js/controllers/wovfnCtrl.js
+++ js/controllers/wovfnCtrl.js
@@ -32,19 +32,28 @@ myApp.config(['$routeProvider', function($routeProvider) {
 /**
  * AngularJS controller which controls logic of app
  */
-myApp.controller('WovfnCtrl', ['$scope', function($scope) {
+myApp.controller('WovfnCtrl', ['$scope', '$location', function($scope, $location) {
   console.log("Switching pages...");
   
-  $scope.navLocations = [{ id: 1, name: "About", link: "#/"},
-			 { id: 2, name: "Portfolio", link: "#/portfolio"},
-			 { id: 3, name: "Resume", link: "#/resume"},
-			 { id: 4, name: "Contact", link: "#/contact"}];
+  $scope.navLocations = [{ id: 1, name: "About", link: "#/", path: "/"},
+			 { id: 2, name: "Portfolio", link: "#/portfolio", path: "/portfolio"},
+			 { id: 3, name: "Resume", link: "#/resume", path: "/resume"},
+			 { id: 4, name: "Contact", link: "#/contact", path: "/contact"}];
 
-  // Start off on About page
-  $scope.idActiveLocation = 1;
+  $scope.activeLocation = $location.path();
   
-  $scope.setSelected = function(idSelectedLocation) {
-      $scope.idActiveLocation = idSelectedLocation;
+  $scope.init = function() {
+      if ($location.path() === "") {
+	  // Start off on the About page
+	  $scope.activeLocation = "/";
+      } else {
+	  // In case the user refreshes the page, set active tab to current location
+	  $scope.activeLocation = $location.path();
+      }
+  }
+  
+  $scope.setSelected = function(selectedLocation) {
+      $scope.activeLocation = selectedLocation;
   }
 
 }]);
